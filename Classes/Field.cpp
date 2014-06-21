9c1e92fea293ab37757379da1a20f355489ffcd6
Andrey Naboka <and.naboka@gmail.com>
2014-06-21 15:51:02 +0400

    Add field background

--- Classes/Field.cpp
+++ Classes/Field.cpp
@@ -19,6 +19,15 @@ Field::Field(cocos2d::Layer* layer)
             (*newPiece)->setCoords(w, h);
         }
     }
+    
+    cocos2d::Sprite* background = cocos2d::Sprite::create("background.png");
+    cocos2d::Size visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
+    const float backgroundWidthScale = visibleSize.width / background->getContentSize().width;
+    const float backgroundHeightScale = visibleSize.height / background->getContentSize().height;
+    background->setScale(backgroundWidthScale, backgroundHeightScale);
+    cocos2d::Point origin = cocos2d::Director::getInstance()->getVisibleOrigin();
+    background->setPosition(origin.x + (visibleSize.width/2), origin.y + (visibleSize.height/2));
+    mLayer->addChild(background);
 }
 
 
